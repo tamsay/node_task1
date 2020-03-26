2ed4d014f424898c51cebebf3102b172af5f6e49
tamsay <longetemitope@gmail.com>
2020-03-26 13:59:14 +0100

    Solution to the first Node task - Simple html form

---
+++ formServer.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const { parse } = require('querystring');
+
+
+const server = http.createServer((request, response)=>{
+    response.writeHead(200, {'Content-Type': 'text/html'});
+	
+    fs.readFile('form.html', 'utf8', function(error, data) {
+        response.write(data);
+    
+
+    if (request.method === 'POST') {
+        collectRequestData(request, result => {
+            console.log(result);
+                fs.writeFile('message.txt', result.message, (error)=>{
+                 console.log(`Inputted message is: ${result.message}`);
+            });
+            response.end();
+        });
+      };
+   response.end();
+  });
+});
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
+
+
+server.listen(8080);
+
